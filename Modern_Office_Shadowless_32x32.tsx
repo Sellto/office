7d90eaf86e68433fd766e4083226891ae59bba21
t.selleslagh <t.selleslagh@stratos-solution.com>
2023-11-12 20:18:28 +0100

    try embed tileset

--- Modern_Office_Shadowless_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Modern_Office_Shadowless_32x32" tilewidth="32" tileheight="32" tilecount="848" columns="16">
- <image source="../../Downloads/Modern_Office_Revamped/3_Modern_Office_Shadowless/Modern_Office_Shadowless_32x32.png" width="512" height="1696"/>
-</tileset>
--- modern office interior.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="modern office interior" tilewidth="32" tileheight="32" tilecount="1424" columns="16">
- <image source="tilesets/Interiors_free_32x32.png" width="512" height="2848"/>
-</tileset>
